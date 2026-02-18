da1c0859e685fad1e625eccd38576f9e458bca8f
K-Piask <kamil.piaskowski@onet.pl>
2026-02-18 14:24:33 +0100

    feat: connect prisma to server and add expenses endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,59 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
 
+const { PrismaClient } = require("./generated/prisma");
+const { PrismaPg } = require("@prisma/adapter-pg")
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => {
     res.json({ ok: true, message: "Backend działa 🚀" });
+});
+
+app.get("/expenses", async (req, res) => {
+    try {
+        const expenses = await prisma.expense.findMany({
+            orderBy: { date: "desc" },
+        });
+        res.json(expenses);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Błąd pobierania wydatków" });
+    }
+});
+
+app.post("/expenses", async (req, res) => {
+    try {
+        const { amount, date, note } = req.body;
+
+        if (!amount || !date) {
+            return res.status(400).json({ error: "amount i date są wymagane" });
+        }
+
+        const expense = await prisma.expense.create({
+            data: {
+                amount: Number(amount),
+                date: new Date(date),
+                note: note ? String(note) : null,
+                userId: 1, //tymczasowo
+            },
+        });
+        res.status(201).json(expense);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Błąd dodawania wydatku" });
+    }
 });
 
 const PORT = 3000;
